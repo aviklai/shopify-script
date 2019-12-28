80702fbe3e5e7cbd0794076d9448444cca684d33
Avi <aviklaiman@hotmail.com>
2019-12-28 22:44:56 +0200

    Finding galleries

--- getProduct.js
+++ getProduct.js
@@ -1,17 +1,25 @@
+function findGallery() {
+    if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
+        return {container: '.Product__Gallery', element: '.Product__SlideItem'}
+    }
+    return null;
+}
+
 var productId = null;
 if (ShopifyAnalytics && ShopifyAnalytics.meta && ShopifyAnalytics.meta.product) {
-    productId = ShopifyAnalytics.meta.product.id;
-    var el = document.createElement('script');
-    el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
-    el.onload = ()=> {
-        console.log(productId);
-        console.log(Shopify.shop)
-        window.RestARInjector.init({
-            container: ".Product__Gallery",
-            element: ".Product__SlideItem",
-            productId: 4127557779542,
-            models_list: "secretoshape"
-        })
-    }
-    document.head.appendChild(el);
+    var gallery = findGallery();
+    if (gallery !== null) {
+        productId = ShopifyAnalytics.meta.product.id;
+        var el = document.createElement('script');
+        el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+        el.onload = ()=> {
+            window.RestARInjector.init({
+                container: gallery.container,
+                element: gallery.element,
+                productId: 4127557779542,
+                models_list: "secretoshape"
+            })
+        }
+        document.head.appendChild(el);
+    }    
 }
