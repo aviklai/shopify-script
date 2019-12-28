174c3bdd5ed694057ae761dee5663bd156bf0abd
Avi <aviklaiman@hotmail.com>
2019-12-28 22:46:54 +0200

    iffy

--- getProduct.js
+++ getProduct.js
@@ -1,25 +1,27 @@
-function findGallery() {
-    if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
-        return {container: '.Product__Gallery', element: '.Product__SlideItem'}
-    }
-    return null;
-}
-
-var productId = null;
-if (ShopifyAnalytics && ShopifyAnalytics.meta && ShopifyAnalytics.meta.product) {
-    var gallery = findGallery();
-    if (gallery !== null) {
-        productId = ShopifyAnalytics.meta.product.id;
-        var el = document.createElement('script');
-        el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
-        el.onload = ()=> {
-            window.RestARInjector.init({
-                container: gallery.container,
-                element: gallery.element,
-                productId: 4127557779542,
-                models_list: "secretoshape"
-            })
+(function(){
+    function findGallery() {
+        if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
+            return {container: '.Product__Gallery', element: '.Product__SlideItem'}
         }
-        document.head.appendChild(el);
-    }    
-}
+        return null;
+    }
+
+    var productId = null;
+    if (ShopifyAnalytics && ShopifyAnalytics.meta && ShopifyAnalytics.meta.product) {
+        var gallery = findGallery();
+        if (gallery !== null) {
+            productId = ShopifyAnalytics.meta.product.id;
+            var el = document.createElement('script');
+            el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+            el.onload = ()=> {
+                window.RestARInjector.init({
+                    container: gallery.container,
+                    element: gallery.element,
+                    productId: 4127557779542,
+                    models_list: "secretoshape"
+                })
+            }
+            document.head.appendChild(el);
+        }    
+    }
+})();
