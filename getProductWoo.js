f8e3e489ae0b0a570212a672a128801a53bbb78b
Avi <aviklaiman@hotmail.com>
2019-12-31 00:20:24 +0200

    Added product id

--- getProductWoo.js
+++ getProductWoo.js
@@ -10,17 +10,20 @@
         var productId = null;
         var gallery = findGallery();
         if (gallery !== null) {
-            var el = document.createElement('script');
-            el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
-            el.onload = ()=> {
-                window.RestARInjector.init({
-                    container: gallery.container,
-                    element: gallery.element,
-                    productId: 4127557779542,
-                    models_list: "secretoshape"
-                })
-            }
-            document.head.appendChild(el);
+            if (document.querySelectorAll('[name="add-to-cart"]')[0].length > 0) {
+                productId = document.querySelectorAll('[name="add-to-cart"]')[0].value;
+                var el = document.createElement('script');
+                el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+                el.onload = ()=> {
+                    window.RestARInjector.init({
+                        container: gallery.container,
+                        element: gallery.element,
+                        productId: 4127557779542,
+                        models_list: "secretoshape"
+                    })
+                }
+                document.head.appendChild(el);
+            }            
         }    
     }, 1000);    
 })();
