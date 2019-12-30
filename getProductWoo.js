20db474dfacd04c1f2b7240d1baec9d35cb7a8b7
Avi <aviklaiman@hotmail.com>
2019-12-30 22:52:53 +0200

    Woo

---
+++ getProductWoo.js
@@ -0,0 +1,27 @@
+(function(){
+    function findGallery() {
+        if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
+            return {container: '.Product__Gallery', element: '.Product__SlideItem'};
+        }else if (document.getElementsByClassName('product-single__photo-wrapper').length > 0 && document.getElementsByClassName('product-single__photo').length > 0) {
+            return {container: '.product-single__photo-wrapper', element: '.product-single__photo'};
+        }
+        return null;
+    }
+
+    var productId = null;
+    var gallery = findGallery();
+    if (gallery !== null) {
+        productId = ShopifyAnalytics.meta.product.id;
+        var el = document.createElement('script');
+        el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+        el.onload = ()=> {
+            window.RestARInjector.init({
+                container: gallery.container,
+                element: gallery.element,
+                productId: 4127557779542,
+                models_list: "secretoshape"
+            })
+        }
+        document.head.appendChild(el);
+    }    
+})();
