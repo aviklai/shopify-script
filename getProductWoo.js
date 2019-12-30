da108e55f6306cc415798292d03df10d401bddab
Avi <aviklaiman@hotmail.com>
2019-12-30 23:01:23 +0200

    added timeput

--- getProductWoo.js
+++ getProductWoo.js
@@ -6,19 +6,21 @@
         return null;
     }
 
-    var productId = null;
-    var gallery = findGallery();
-    if (gallery !== null) {
-        var el = document.createElement('script');
-        el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
-        el.onload = ()=> {
-            window.RestARInjector.init({
-                container: gallery.container,
-                element: gallery.element,
-                productId: 4127557779542,
-                models_list: "secretoshape"
-            })
-        }
-        document.head.appendChild(el);
-    }    
+    setTimeout(function() {
+        var productId = null;
+        var gallery = findGallery();
+        if (gallery !== null) {
+            var el = document.createElement('script');
+            el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+            el.onload = ()=> {
+                window.RestARInjector.init({
+                    container: gallery.container,
+                    element: gallery.element,
+                    productId: 4127557779542,
+                    models_list: "secretoshape"
+                })
+            }
+            document.head.appendChild(el);
+        }    
+    }, 1000);    
 })();
