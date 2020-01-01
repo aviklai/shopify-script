f0112527a7745f97ddf53c5d96f6802a2435e9ac
Avi <aviklaiman@hotmail.com>
2020-01-01 22:04:33 +0200

    Adding wix

---
+++ getProdcutWix.js
@@ -0,0 +1,29 @@
+(function(){
+    function findGallery() {
+        if (document.getElementsByClassName('woocommerce-product-gallery').length > 0 && document.getElementsByClassName('woocommerce-product-gallery__image').length > 0) {
+            return {container: '.woocommerce-product-gallery', element: '.woocommerce-product-gallery__image'};
+        }
+        return null;
+    }
+
+    setTimeout(function() {
+        var productId = null;
+        var gallery = findGallery();
+        if (gallery !== null) {
+            if (document.querySelectorAll('[name="add-to-cart"]')[0].length > 0) {
+                productId = document.querySelectorAll('[name="add-to-cart"]')[0].value;
+                var el = document.createElement('script');
+                el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+                el.onload = ()=> {
+                    window.RestARInjector.init({
+                        container: gallery.container,
+                        element: gallery.element,
+                        productId: 4127557779542,
+                        models_list: "secretoshape"
+                    })
+                }
+                document.head.appendChild(el);
+            }            
+        }    
+    }, 1000);    
+})();
