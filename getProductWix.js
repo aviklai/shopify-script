67e1bb0e5c5325e3f926df32eefde505e81aba5f
Avi <aviklaiman@hotmail.com>
2020-01-02 23:09:17 +0200

    WIP

---
+++ getProductWix.js
@@ -0,0 +1,23 @@
+(function(){
+    function findGallery() {
+        if (document.querySelectorAll('[data-hook="slider-gallery-slide"]').length > 0 && document.querySelectorAll('[data-hook="product-thumbnail-wrapper"]').length > 0) {
+            return {container: '[data-hook="slider-gallery-slide"]', element: '[data-hook="product-thumbnail-wrapper"]'};
+        }
+        return null;
+    }    
+    var productId = null;
+    var gallery = findGallery();
+    if (gallery !== null) {
+        var el = document.createElement('script');
+        el.setAttribute('src', 'https://v2.rest-ar.com/restar-injector.js')
+        el.onload = ()=> {
+            window.RestARInjector.init({
+                container: gallery.container,
+                element: gallery.element,
+                productId: 4127557779542,
+                models_list: "secretoshape"
+            })
+        }
+        document.head.appendChild(el);          
+    }        
+})();
