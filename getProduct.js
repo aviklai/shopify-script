acfb829db18955b25ddf193e310dfa48a7a6a4b6
Avi <aviklaiman@hotmail.com>
2020-01-03 01:13:11 +0200

    t

--- getProduct.js
+++ getProduct.js
@@ -3,7 +3,6 @@
         if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
             return {container: '.Product__Gallery', element: '.Product__SlideItem'};
         }else if (document.getElementsByClassName('product-single__photo-wrapper').length > 0 && document.getElementsByClassName('product-single__photo').length > 0) {
-            document.getElementsByClassName('product-single__photo')[0].style.paddingTop = "0px";
             document.getElementsByClassName('product-single__photo')[0].style.height = document.getElementsByClassName('product-single__photo')[0].offsetHeight + 'px';
             document.getElementsByClassName('product-single__photo')[0].style.width = document.getElementsByClassName('product-single__photo')[0].offsetWidth + 'px';
             return {container: '.product-single__photo-wrapper', element: '.product-single__photo'};
@@ -24,7 +23,8 @@
                     element: gallery.element,
                     productId: 4127557779542,
                     models_list: "secretoshape"
-                })                
+                })    
+                document.getElementsByClassName('product-single__photo')[0].style.paddingTop = "0px";
             }
             document.head.appendChild(el);
         }    
