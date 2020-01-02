de8184b71f4115bed9f51c67f44a491cd14de14f
Avi <aviklaiman@hotmail.com>
2020-01-03 01:17:02 +0200

    WIP

--- getProduct.js
+++ getProduct.js
@@ -3,8 +3,9 @@
         if (document.getElementsByClassName('Product__Gallery').length > 0 && document.getElementsByClassName('Product__SlideItem').length > 0) {
             return {container: '.Product__Gallery', element: '.Product__SlideItem'};
         }else if (document.getElementsByClassName('product-single__photo-wrapper').length > 0 && document.getElementsByClassName('product-single__photo').length > 0) {
-            // document.getElementsByClassName('product-single__photo')[0].style.height = document.getElementsByClassName('product-single__photo')[0].offsetHeight + 'px';
-            // document.getElementsByClassName('product-single__photo')[0].style.width = document.getElementsByClassName('product-single__photo')[0].offsetWidth + 'px';
+            document.getElementsByClassName('product-single__photo')[0].style.height = document.getElementsByClassName('product-single__photo')[0].offsetHeight + 'px';
+            document.getElementsByClassName('product-single__photo')[0].style.width = document.getElementsByClassName('product-single__photo')[0].offsetWidth + 'px';
+            document.getElementsByClassName('product-single__photo')[0].style.paddingTop = "0px";            
             return {container: '.product-single__photo-wrapper', element: '.product-single__photo'};
         }
         return null;
@@ -24,7 +25,6 @@
                     productId: 4127557779542,
                     models_list: "secretoshape"
                 })    
-                document.getElementsByClassName('product-single__photo')[0].style.paddingTop = "0px";
             }
             document.head.appendChild(el);
         }    
