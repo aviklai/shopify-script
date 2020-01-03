670ec73edd1d333a8be10dd8e51711b28656eb8e
Avi <aviklaiman@hotmail.com>
2020-01-03 14:30:48 +0200

    Refactored gallery factory

--- getProduct.js
+++ getProduct.js
@@ -1,29 +1,34 @@
 (function(){
-    var gallery1 = {container: '.Product__Gallery', element: '.Product__SlideItem'};
-    var gallery2 = {container: '.product-single__photo-wrapper', element: '.product-single__photo', preAdjustments: function() { 
-        document.querySelectorAll(this.element)[0].style.height = document.querySelectorAll(gallery2.element)[0].offsetHeight + 'px';
-        document.querySelectorAll(this.element)[0].style.width = document.querySelectorAll(gallery2.element)[0].offsetWidth + 'px';                
-        document.querySelectorAll(this.element)[0].style.paddingTop = "0px"; 
-    }};
     
+    function getGalleries() {
+        return [
+            {container: '.Product__Gallery', element: '.Product__SlideItem'},
+            {container: '.product-single__photo-wrapper', element: '.product-single__photo', preAdjustments: function() { 
+                document.querySelectorAll(this.element)[0].style.height = document.querySelectorAll(gallery2.element)[0].offsetHeight + 'px';
+                document.querySelectorAll(this.element)[0].style.width = document.querySelectorAll(gallery2.element)[0].offsetWidth + 'px';                
+                document.querySelectorAll(this.element)[0].style.paddingTop = "0px"; 
+            }}
+        ]
+    }  
+
     function Gallery(container, element, preAdjustments) {
         this.container = container;
         this.element = element;
         this.preAdjustments = preAdjustments ? preAdjustments : function() {};
-    }
+    }       
 
-    function galleryFactory() {
-        if (document.querySelectorAll(gallery1.container).length > 0 && document.querySelectorAll(gallery1.element).length > 0) {
-            return new Gallery(gallery1.container, gallery1.element);
-        }else if (document.querySelectorAll(gallery2.container).length > 0 && document.querySelectorAll(gallery2.element).length > 0) {
-            return new Gallery(gallery2.container, gallery2.element, gallery2.preAdjustments);
+    function galleryFactory(galleries) {
+        for (let index = 0; index < galleries.length; index++) {
+           if (document.querySelectorAll(galleries[index].container).length > 0 && document.querySelectorAll(galleries[index].element).length > 0) {
+               return new Gallery(galleries[index].container, galleries[index].element, galleries[index].preAdjustments);
+           }            
         }
-        return null;
+        return null;        
     }    
  
     var productId = null;
     if (ShopifyAnalytics && ShopifyAnalytics.meta && ShopifyAnalytics.meta.product) {
-        var gallery = galleryFactory();
+        var gallery = galleryFactory(getGalleries());
         if (gallery !== null) {
             gallery.preAdjustments();
             productId = ShopifyAnalytics.meta.product.id;
