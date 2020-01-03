6f13e37fd28b62e979f847f76911bbaa4d9323cd
Avi <aviklaiman@hotmail.com>
2020-01-03 15:13:06 +0200

    Another gallery

--- getProduct.js
+++ getProduct.js
@@ -13,7 +13,8 @@
             {container: '.product-single__featured-image-wrapper', element: '.product-single__photos', preAdjustments: function() { commonElementAdjustment(this.element) }},
             {container: '.product__photo', element: '.product__photo--single', preAdjustments: function() { commonElementAdjustment(this.element) }},
             {container: '.photos__item', element: '.product-single__photo', preAdjustments: function() { commonElementAdjustment(this.element) }},
-            {container: '.lazyload__image-wrapper', element: '.product__image-wrapper', preAdjustments: function() { commonElementAdjustment(this.element) }}
+            {container: '.lazyload__image-wrapper', element: '.product__image-wrapper', preAdjustments: function() { commonElementAdjustment(this.element) }},
+            {container: '.product-single__photo--container', element: '.product-single__photo-wrapper', preAdjustments: function() { commonElementAdjustment(this.element) }}
         ]
     }  
 
